60045cc406a001e37fb4a680f8fb3ae9538c5ea2
Catia Brehm <catiabrehm@AitakoMac.home>
2023-03-30 18:27:51 +0100

    final script

--- script.js
+++ script.js
@@ -4,19 +4,17 @@ const refreshBtn = document.querySelector(".refresh-btn");
 const maxPaletteBoxes = 32;
 
 const generatePalette = () => {
-    container.innerHTML = ""; //! clearing container
+    container.innerHTML = "";
 
     for (let i = 0; i < maxPaletteBoxes; i++) {
-        //!generating a random hex color code
+
         let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
         randomHex = `#${randomHex.padStart(6, '0')}`;
 
-        //! creating new li element and inserting it container 
         const color = document.createElement('li');
         color.classList.add('color');
         color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
                            <span class="hex-value">${randomHex}</span>`;
-        //! add click event to current li elemnt to copy color
         color.addEventListener('click', () => copyColor(color, randomHex));
         container.appendChild(color);
     }
